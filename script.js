9e1735072cd3bd2b8e1467d6ec514c8c5d0745c3
Mario <mario.tota.mt@gmail.com>
2021-01-12 12:42:30 +0100

    commit

---
+++ script.js
@@ -0,0 +1,46 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    linkHeader: [
+      {
+        name: 'home',
+        link: 'https://i.pinimg.com/236x/70/2a/a0/702aa076cc2137ea971257f78c4b5269.jpg'
+      },
+      {
+        name: 'pages',
+        link: 'https://www.napolitan.it/wp-content/uploads/2017/10/ssimg_235-1024x683.jpg',
+      },
+      {
+        name: 'program',
+        link: 'https://static2-viaggi.corriereobjects.it/wp-content/uploads/2015/06/giappone-1080x720.jpg?v=134538'
+      },
+      {
+        name: 'tickets',
+        link: 'https://cameranation.it/wp-content/uploads/2020/03/paesaggi.jpg'
+      },
+      {
+        name: 'speakers',
+        link: 'https://dbdzm869oupei.cloudfront.net/img/photomural/large/5df11869474cd.jpg'
+      },
+      {
+        name: 'papers',
+        link: 'https://www.reterurale.it/flex/images/6/6/8/D.f2c2072928623596b6ad/1.jpg'
+      },
+      {
+        name: 'blog',
+        link: 'https://www.raiplayradio.it/cropgd/853x480/dl/img/2018/04/26/1524742758393_paesaggio.jpg'
+      },
+      {
+        name: 'shortcodes',
+        link: 'https://www.jovittorio.com/wp-content/uploads/2018/01/fotografare-paesaggi-1.jpg'
+      }
+    ]
+	},
+  methods: {
+
+  },
+  mounted:
+    function () {
+
+    }
+});
